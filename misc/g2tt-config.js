b46e564ebfe93e49c427a50deff4f7b2e9ab5cf0
Dis McCarthy <github@sigkill.net>
2013-06-25 12:33:39 -0400

    typo fixes and add the config file

---
+++ misc/g2tt-config.js
@@ -0,0 +1,9 @@
+// Config
+global_ttrssUrl = "/";
+
+// Preferences
+pref_Feed = "-4"; // Default: all items
+pref_IsCat = false;
+pref_ViewMode = 'unread'; // Default: unread articles only
+pref_TextType = "content"; // Default: full articles
+pref_OrderBy = 'date'; // Default: newest first
